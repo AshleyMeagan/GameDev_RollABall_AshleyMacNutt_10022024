dd7234b7986b9a870278668267d22aeeab473431
AshleyMeagan <macnutta@live.com>
2024-10-03 13:08:38 -0300

    Can collect PickUp objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,5 +26,12 @@ void FixedUpdate()
     Vector3 movement = new Vector3(movementX, 0.0f, movementY);
     rb.AddForce(movement * speed);
 }
+void OnTriggerEnter(Collider other)
+{
+    if (other.gameObject.CompareTag("PickUp")) 
+{
+    other.gameObject.SetActive(false);
+}
+}
 }
 
